30417e5b86cbb0d4cb3bdc5f9175f560d2166f83
Onyi Dozie <onyiyechukwu.dozie@kcl.ac.uk>
2023-12-03 14:54:25 +0000

    solve prime v2

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    number = 1
+    if(number_of_primes < 1):
+        raise ValueError(f"number_of_primes = {number_of_primes}it should be a positive number")
+    while(len(list) < number_of_primes):
+        number = number + 1
+        if(isAPrime(number)):
+            list.append(number)
     return list
+
+
+def isAPrime(x):
+    aPrime = True;
+    for i in range(2,x):
+        if(x % i ) == 0:
+            aPrime = False;
+    return aPrime
